1fbf9ca5c8f654ac33917c6627595e37c0bac453
bryanwilie <bryanwilie9292@gmail.com>
2022-08-08 17:01:20 +0800

    add transformers examples on run_glue

---
+++ run_glue.sh
@@ -0,0 +1,13 @@
+export TASK_NAME=mrpc
+
+TOKENIZERS_PARALLELISM=false
+CUDA_VISIBLE_DEVICES=5 python run_glue.py \
+  --model_name_or_path bert-base-cased \
+  --task_name $TASK_NAME \
+  --do_train \
+  --do_eval \
+  --max_seq_length 128 \
+  --per_device_train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 3 \
+  --output_dir ./save/$TASK_NAME/
